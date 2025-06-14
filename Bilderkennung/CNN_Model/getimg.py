9a3900d06f2f3da24585744f65f33873d41f8c5e
NHeinrich <niklasheinrich1501@gmail.com>
2025-06-14 13:08:16 +0200

    nochmal?

--- Bilderkennung/CNN_Model/getimg.py
+++ Bilderkennung/CNN_Model/getimg.py
@@ -2,20 +2,22 @@ import time
 import cv2 as cv
 from screentext import ScreenText
 from fpstracker import FPSTracker
-from serialcomm import passtoSerial ; from serialcomm import checkresponse
+from serialcomm import comm
 import inference as RCNN
 
 class Object():
-    def __init__(self, x1, y1, x2, y2, label):
+    def __init__(self, x1, y1, x2, y2, label, confidence):
         self.x1 = x1
         self.y1 = y1
         self.x2 = x2
         self.y2 = y2
         self.label = label
+        self.confidence = confidence
     def __str__(self):
         getlabeltext = lambda x:"Glass" if x==1 else "Hand"
-        return f"Object: {getlabeltext(self.label)} ({self.x1}, {self.y1}, {self.x2}, {self.y2})"
+        return f"Object: {getlabeltext(self.label)} (({self.x1}, {self.y1}), ({self.x2}, {self.y2})), {self.confidence}"
 
+#check whether array contains objects with a specific label
 def checkforObject(objects, searchlabel):
     for o in objects:
         if o.label == searchlabel:
@@ -27,6 +29,15 @@ def drawvisuals(img, x1, y1, x2, y2, text):
     cv.putText(img, text, (x1+5,y1+15), cv.FONT_HERSHEY_SIMPLEX,
             0.5, (0, 255, 0), 1, cv.LINE_AA)
     
+def update_fps():
+    fps = fpstracker.updateFPS()
+    fps_text.set_Text(f"FPS: {fps:.0f}")
+    fps_text.set_Position((10,20))
+    fps_text.set_Color((0,0,255))
+    fps_text.showText(frame)
+
+#get arduino
+arduino = comm('COM3')
 
 fps_text = ScreenText()
 fpstracker = FPSTracker()
@@ -34,9 +45,10 @@ model = RCNN.initmodel()
 objects = []
 waitingforUNO = False
 runtime = 0
-score_threshold = 0.5
+score_threshold = 0.9
+
 # Kamera öffnen (0 für die erste erkannte Kamera)
-cap = cv.VideoCapture(0, cv.CAP_DSHOW)
+cap = cv.VideoCapture(0)
 
 if not cap.isOpened():
     print("Error: capture not open")
@@ -48,44 +60,38 @@ while True:
         print("Error: no frame")
         break
     
-    #actually pre-process the image
+    #pre-process the image
     frame = cv.resize(frame, (640, 480))#make sure every frame has the same size, otherwise dependent on camera
     #frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)#apply grayscale, color not needed for shape detection, reduces size
     
-    #implement frames-per-second counter
-    fps = fpstracker.updateFPS()
-    fps_text.set_Text(f"FPS: {fps:.0f}")
-    fps_text.set_Position((10,20))
-    fps_text.set_Color((0,0,255))
-    fps_text.showText(frame)
-    
     if not waitingforUNO:
-        #implement MLM
+        #pass img to RCNN model
         start_time = time.time()
         output = RCNN.checkimg(model, frame)
         runtime = time.time() - start_time
         print(runtime)
         print(output)
         prediction = output[0]
+        
         #if cup is found, get cup coordinates
         for box,  label, score, in zip(prediction['boxes'], prediction['labels'], prediction['scores']):
             if score.item()>=score_threshold:
                 x1, y1, x2, y2 = box.int().tolist()
-                objects.append(Object(x1, y1, x2, y2, label.item()))
-                getlabeltext = lambda x:"Glass" if x==1 else "Hand"
-                text = f"{getlabeltext(label.item())} ({score:.2f})"
+                objects.append(Object(x1, y1, x2, y2, label.item(), score.item()))
+                text = objects[len(objects)].str()
                 drawvisuals(frame, x1, y1, x2, y2, text)
-        #pass coordinate to Arduino
+        
+        #pass coordinate to Arduino if no hand in img
         if checkforObject(objects, 1) and not checkforObject(objects, 2):
-            #get coordinates of the center of the cup
+            #get coordinates of the center of the first cup
             xmid = int(objects[0].x1) + 0.5*(objects[0].x2-objects[0].x1)
             ymid = int(objects[0].y1) + 0.5*(objects[0].y2-objects[0].y1)
-            passtoSerial(xmid, ymid)
+            arduino.passtoSerial(xmid, ymid)
             waitingforUNO = True
     else:
-        if checkresponse():
+        if arduino.checkresponse()!=None:
             waitingforUNO = False
-            objects.remove(objects[0])
+            objects.clear()
 
     #show pre-processed frame
     cv.imshow('Livebild', frame)
